0510447e9a837bc67539b346430df83dc621beb3
vdurgule <vdurgule31@gmail.com>
2024-02-07 00:26:35 +0530

    add Loop

---
+++ loop/loopFrstTut.py
@@ -0,0 +1,13 @@
+# Simple Python program using a loop to print numbers from 1 to 10
+
+# Using a for loop
+print("Using a for loop:")
+for i in range(1, 11):
+    print(i)
+
+# Using a while loop
+print("\nUsing a while loop:")
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
